f789166f85e495abbaf0ca2b96e18f9384d5e0ba
SuperSynthWave <synthwaveswitch@gmail.com>
2023-04-03 22:32:18 +0530

    Initial commit

---
+++ AppDomainTestApp/AppDomainTestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDomainTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain appDomain = AppDomain.CreateDomain("MyAppDomain");
+
+            AppDomainTestLib.MyClass myClassObj = (AppDomainTestLib.MyClass)appDomain.CreateInstanceFromAndUnwrap(@"C:\Users\Gagan\Desktop\AppDomainTestLib\AppDomainTestLib\bin\Debug\AppDomainTestLib.dll", typeof(AppDomainTestLib.MyClass).FullName);
+            Console.WriteLine("App Domain created");
+
+            myClassObj.setClassName("gagan's_class");
+            myClassObj.setIDName("test1");
+            Console.WriteLine(myClassObj.className);
+
+            AppDomain newAppDomain = AppDomain.CreateDomain("NewAppDomain");
+            AppDomainTestLib.MyClass newClassObj = (AppDomainTestLib.MyClass)newAppDomain.CreateInstanceFromAndUnwrap(@"C:\Users\Gagan\Desktop\AppDomainTestLib\AppDomainTestLib\bin\Debug\AppDomainTestLib.dll", typeof(AppDomainTestLib.MyClass).FullName);
+            newClassObj.setClassName("gagan's_new_class");
+            newClassObj.setIDName("test235");
+            Console.WriteLine(newClassObj.className);
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine(myClassObj.getIdName() + "\n");
+            Console.WriteLine(newClassObj.getIdName() + "\n");
+            myClassObj.getStaticVars();
+            myClassObj.setStaticVars("obj1");
+            myClassObj.getStaticVars();
+            newClassObj.getStaticVars();
+            newClassObj.setStaticVars("obj2");
+            newClassObj.getStaticVars();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AppDomainTestLib/AppDomainTestLib/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDomainTestLib
+{
+    public class MyClass : MarshalByRefObject
+    {
+        public string className { set; get; }
+        public static string idName = "NO_ID";
+
+        public MyClass()
+        {
+
+        }
+
+        public void setClassName(string classnm)
+        {
+            this.className = classnm;
+        }
+
+        public void setIDName(string idname)
+        {
+            idName = idname;
+        }
+
+        public string getIdName()
+        {
+            return idName;
+        }
+
+        public void setStaticVars(string value1)
+        {
+            MyStaticClass.val1 = value1;
+            MyStaticClass.inited = true;
+        }
+
+        public void getStaticVars()
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine(MyStaticClass.inited.ToString() + "\n");
+            Console.WriteLine(MyStaticClass.val1);
+        }
+
+    }
+
+    public static class MyStaticClass{
+        public static string val1 = "NO_VAL";
+        public static bool inited = false;
+
+}
+
+}
